3be561bdbc6463fb09ff4acc15958168cb06ebf5
Naasor <amofasandra7@gmail.com>
2023-07-18 14:23:14 -0700

    prototypes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,10 @@
+#ifndef main_h
+#define main_h
+
 /*_putchar - writes the character c to stdout*/
 int _putchar(char c);
+/*function that prints the alphabet, in lowercase*/
+void print_alphabet(void);
+
+
+#endif
