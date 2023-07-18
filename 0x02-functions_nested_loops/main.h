081b9323fc8a1cc3f24a5b6f6700b1698d857356
Naasor <amofasandra7@gmail.com>
2023-07-18 16:56:48 -0700

    prototype

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,5 +12,18 @@ int _islower(int c);
 /*function that checks for alphabetic character*/
 int _isalpha(int c);
 
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
 
 #endif
