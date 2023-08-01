fcbc4b2579c155d3e22335169d69ab69e82e5bdd
Naasor <amofasandra7@gmail.com>
2023-07-31 17:03:25 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strpbrk - Function that searches a string for any of a set of bytes
@@ -20,5 +21,5 @@ char *_strpbrk(char *s, char *accept)
 		}
 		s++;
 	}
-	return ('\0');
+	return (NULL);
 }
