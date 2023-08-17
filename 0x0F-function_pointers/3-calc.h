f62ae56e22992dacd9c8175e2098bfc78564834e
Naasor <amofasandra7@gmail.com>
2023-08-16 18:05:17 -0700

    updated task3 header file

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,8 @@
 #ifndef CALC_H
 #define CALC_H
 
+#include <stddef.h>
+
 /**
  * struct op - Struct op
  *
@@ -17,7 +19,7 @@ typedef struct op
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
-op_div(int a, int b);
+int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
